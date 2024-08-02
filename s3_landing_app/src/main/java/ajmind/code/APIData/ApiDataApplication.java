0fd8c72d4b2c0ad6fe87fe039d324ca6c9aab54e
Adam Maas <adamjm1220@gmail.com>
2024-08-02 10:22:46 -0400

    initial_springboot_app

---
+++ s3_landing_app/src/main/java/ajmind/code/APIData/ApiDataApplication.java
@@ -0,0 +1,13 @@
+package ajmind.code.APIData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDataApplication.class, args);
+	}
+
+}
---
+++ s3_landing_app/src/test/java/ajmind/code/APIData/ApiDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ajmind.code.APIData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
