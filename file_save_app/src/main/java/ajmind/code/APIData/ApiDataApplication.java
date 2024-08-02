44df58e94990d116c520cebf9a57d69918e190a7
Adam Maas <adamjm1220@gmail.com>
2024-08-02 12:19:42 -0400

    LandDataLocallyToDesktop

---
+++ file_save_app/src/main/java/ajmind/code/APIData/ApiDataApplication.java
@@ -0,0 +1,52 @@
+package ajmind.code.APIData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
+
+import ajmind.code.APIData.api.Todo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+@SpringBootApplication
+public class ApiDataApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ApiDataApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDataApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    @Profile("!test")
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Todo todo = restTemplate.getForObject(
+                    "https://jsonplaceholder.typicode.com/todos/1", Todo.class);
+			//Add in desktop save location
+            saveToFile(todo, "/Users/adammaas/desktop/todo.json");
+        };
+    }
+//add in addditional code to save to desktop
+    private void saveToFile(Todo todo, String filePath) {
+        try (FileWriter fileWriter = new FileWriter(new File(filePath))) {
+            fileWriter.write(todo.toString());
+            log.info("File saved to: " + filePath);
+        } catch (IOException e) {
+            log.error("Error writing to file", e);
+        }
+    }
+}
---
+++ file_save_app/src/test/java/ajmind/code/APIData/ApiDataApplicationTests.java
@@ -0,0 +1,14 @@
+package ajmind.code.APIData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
